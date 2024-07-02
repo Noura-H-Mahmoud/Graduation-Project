67c8bb6bd2747876f242500fdcce15d2a01b62d3
maya <mayasleman21@gmail.com>
2024-07-02 17:23:25 +0300

    improve

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './App.css'
+import MyAccount from './Pages/Account Flow/MyAccount/MyAccount';
 
 function App() {
   useEffect(() => {
@@ -14,7 +15,8 @@ function App() {
   }, []);
   return (
     <>
-    <Outlet />
+    {/* <Outlet /> */}
+    <MyAccount />
     </>
   )
 }
