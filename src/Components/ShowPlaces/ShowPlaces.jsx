50c6db20fa312c5a372a34da6d42dea9133e1b6a
OlaShahoud <Olashahoud5@gmail.com>
2024-07-05 15:23:01 +0000

    JS

--- src/Components/ShowPlaces/ShowPlaces.jsx
+++ src/Components/ShowPlaces/ShowPlaces.jsx
@@ -9,13 +9,56 @@ import date from"./../../assets/images/date.svg"
 import building from"./../../assets/images/building.svg"
 import user from"./../../assets/images/User.svg"
 import { Link } from 'react-router-dom';
-
+import dayjs from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function ShowPlaces() {
+  let index=1
   const [destination,setDestination]=useState(false)
  const toggleHandler = () => {
     setDestination(!destination)
   };
+  const [room,setRoom]=useState(false)
+  const RoomHandler = () => {
+     setRoom(!room)
+   };
+const[val,setVal]=useState('')
+const click=()=>{
+(newValue) => setVal(newValue)
+alert (val)
+}
+const [count,setcount]=useState(0)
+
+  const OSdataDes=[
+    {des1:'Makkah, Saudi Arabia',des2:'Dubai, United Arab Emirates',des3:'Al Madinah, Saudi Arabia',des4:'Cairo, Egypt'},
+    {des1:'Hurghada, Egypt',des2:'Sharm El Sheikh, Egypt',des3:'Alexandria, Egypt',des4:'Istanbul, Turkey'},
+    {des1:'Abu Dhabi, United Arab Emirates',des2:'Sharjah, United Arab Emirates',des3:'Riyadh, Saudi Arabia',des4:'Jeddah, Saudi Arabia'},
+    {des1:'Ain Sokhna, Egypt',des2:'Beirut, Lebanon',des3:'Dahab, Egypt',des4:'Doha, Qatar'},
+    {des1:'Aswan, Egypt',des2:'Kuwait',des3:'Ajman, United Arab Emirates',des4:'Amman, Jordan'},
+  ] 
+  const OSdataRoom=[
+    {
+      Title:"Adults",
+      year:">17years",
+      btn1contact:"-",
+      btn2contact:"+",
+      class:"OS-Ti-Flex "
+    }, {
+      Title:"Children",
+      year:"<17years",
+      btn1contact:"-",
+      btn2contact:"+",  
+      class:"OS-Ti-Flex OS-Ti-Padding1"
+    },
+    
+   
+  ]
+ 
   return (
     <>
       <div className='MS-showFlight MS-ShowPlaces container'>
@@ -36,34 +79,57 @@ export default function ShowPlaces() {
                 size="large"
                 className="MS-firstfield OS-firstfiled"
                 onClick={toggleHandler}
+             
+               
               />
            
              <div className={destination?"OS-Destination":"OS-DestinationDisplay"}>
-            <h1>popular cities</h1>
+            <h1 className='OS-Destination-h1'>popular cities</h1>
               <div>
-                <div className='OS-DestinationFlex'>
-                <p>Makkah</p>
-                <p>Dubai</p>
-                <p>Al Madinah</p>
-                <p>Cairo</p>
+              {OSdataDes.map((item)=>{
+              return <>
+                <div className='OS-DestinationFlex'  >
+                <div className='OS-Destination-Par'onClick={click}>{item.des1}</div>
+                <p className='OS-Destination-Par'onClick={click}>{item.des2}</p>
+                <p className='OS-Destination-Par'onClick={click}>{item.des3}</p>
+                <p className='OS-Destination-Par'onClick={click}>{item.des4}</p>
                 </div>
-</div>
+                </>})}
+              </div>
              </div>
-              <TextField
+<LocalizationProvider dateAdapter={AdapterDayjs} className='OS-Date-co'>
+      <DemoContainer components={['DatePicker', 'DatePicker']} className='OS-Date-co'>
+        <DatePicker  
+         id="outlined-size-small"
+        label="Check In"
+         defaultValue={dayjs('2022-02-12')}
+         className="MS-field1"  size="large"
+       format=' ddd/MM/YY'
+         /> <DatePicker  
+         id="outlined-size-small"
+        label="Check Out"
+         defaultValue={dayjs('2022-02-12')}
+         className="MS-field1"  size="large"
+         format=' ddd/MM/YY'
+         />
+    </DemoContainer>
+    </LocalizationProvider>
+{/*}  <TextField
                 label="Check In"
                 id="outlined-size-small"
-                defaultValue="Fri 12/2"
                 size="large"
                  className="MS-field1"
+                defaultValue="Fri 12/2"
+                
                 InputProps={{
                   endAdornment: (
                     <InputAdornment position="end">
-                       <img src={date} alt="icon" className="MS-field-icon" />
+                       <img  src={date} alt="icon" className="MS-field-icon" />
                     </InputAdornment>
                   ),
                 }}
-              />
-              <TextField
+              />*/}
+             {/*} <TextField
                 label="Check Out"
                 id="outlined-size-small"
                 defaultValue="Sun 12/4"
@@ -76,13 +142,14 @@ export default function ShowPlaces() {
                       </InputAdornment>
                     ),
                   }}
-              />
+              />*/}
               <TextField
                 label="Rooms & Guests"
                 id="outlined-size-small"
                 defaultValue="1 room, 2 guests"
                 size="large"
-                className="MS-field1"
+                className="MS-field1 OS-firstfiled1"
+                onClick={RoomHandler}
                 InputProps={{
                     startAdornment: (
                         <InputAdornment position="end">
@@ -96,6 +163,33 @@ export default function ShowPlaces() {
                     ),
                   }}
               />
+              <div className={room?"OS-Room":"OS-DestinationDisplay"}>
+            <h1 className='OS-Destination-h1 OS-Room-h1'>Room{index}</h1>
+            <div className='OS-Ti-Flex OS-All-Padding'>
+              {OSdataRoom.map((item)=>{
+              return <>
+              <div className={item.class}>
+               <img src={user} alt="icon" className="MS-field-icon" />
+             <div  className='OS-DivT-Room'>
+              <h6 className='OS-h6-Room'>{item.Title}</h6>
+              <p  className='OS-p-Room'> {item.year} </p>
+              </div>
+              <div className='OS-Ti-Flex'>
+              <button className='OS-btn-Room' onClick={()=> {if(count<=0){setcount(count)}else{setcount(count-1)}}}>{item.btn1contact}</button>
+              <p className='OS-pp-Room'>{count}</p>
+              <button className='OS-btn-Room'onClick={()=>setcount(count+1)}>{item.btn2contact}</button>
+              </div>
+              </div>
+                </>})}
+                </div>
+                <div className='OS-Ti-FlexStyle'>
+                <div className='OS-Ti-Flex OS-Ti-Style'>
+                  <div className='OS-Plus-Style'>+</div>
+                  <h6 className='OS-AddRoom-Style'> add room</h6>
+                  </div>
+                <div className='OS-applyDiv-Style' onClick={RoomHandler}><p className='OS-apply-Style'>Apply</p></div>
+              </div>
+             </div>
             </div>
             <div className="MS-right">
             <a><img src={plus} alt="plus" className="MS-plus" />Add Promo Code</a>
